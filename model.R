8f475fdacaa039a8fb78c5175e838f12488725f9
pherreraariza <phariza@gmail.com>
2017-09-02 23:43:51 +0200

    added NN and SVM

--- model.R
+++ model.R
@@ -7,6 +7,7 @@ library(rpart.plot)
 library(rattle)
 library(randomForest)
 library(corrplot)
+library(nnet)
 
 # set the URL for the download
 UrlTrain <- "http://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv"
@@ -40,7 +41,7 @@ corMatrix <- cor(TrainSet[, -54])
 corrplot(corMatrix, order = "FPC", method = "circle", type = "lower", 
          tl.cex = 0.8, tl.col = rgb(0, 0, 0))
 
-# model fit
+# model fit RF
 set.seed(12345)
 controlRF <- trainControl(method="cv", number=3, verboseIter=FALSE)
 modFitRandForest <- train(classe ~ ., data=TrainSet, method="rf", trControl=controlRF)
@@ -56,7 +57,7 @@ plot(confMatRandForest$table, col = confMatRandForest$byClass,
      main = paste("Random Forest - Accuracy =",
                   round(confMatRandForest$overall['Accuracy'], 4)))
 
-# model fit
+# model fit TREE
 set.seed(12345)
 modFitDecTree <- rpart(classe ~ ., data=TrainSet, method="class")
 fancyRpartPlot(modFitDecTree)
@@ -71,7 +72,7 @@ plot(confMatDecTree$table, col = confMatDecTree$byClass,
      main = paste("Decision Tree - Accuracy =",
                   round(confMatDecTree$overall['Accuracy'], 4)))
 
-# model fit
+# model fit GBM
 set.seed(12345)
 controlGBM <- trainControl(method = "repeatedcv", number = 5, repeats = 1)
 modFitGBM  <- train(classe ~ ., data=TrainSet, method = "gbm",
@@ -86,3 +87,36 @@ confMatGBM
 # plot matrix results
 plot(confMatGBM$table, col = confMatGBM$byClass, 
      main = paste("GBM - Accuracy =", round(confMatGBM$overall['Accuracy'], 4)))
+
+# model fit NN
+set.seed(12345)
+controlNN <- trainControl(method = "repeatedcv", number = 5, repeats = 1)
+modFitNN  <- train(classe ~ ., data=TrainSet, method = "pcaNNet", 
+                    trControl = controlNN, verbose = FALSE)
+modFitNN$finalModel
+
+# prediction on Test dataset
+predictNN <- predict(modFitNN, newdata=TestSet)
+confMatNN <- confusionMatrix(predictNN, TestSet$classe)
+confMatNN
+
+# plot matrix results
+plot(confMatNN$table, col = confMatNN$byClass, 
+     main = paste("Neural Net - Accuracy =", round(confMatNN$overall['Accuracy'], 4)))
+
+
+# model fit SVM
+set.seed(12345)
+controlSVM <- trainControl(method = "repeatedcv", number = 5, repeats = 1)
+modFitSVM  <- train(classe ~ ., data=TrainSet, method = "svmRadial", 
+                   trControl = controlSVM, verbose = FALSE)
+modFitSVM$finalModel
+
+# prediction on Test dataset
+predictSVM <- predict(modFitSVM, newdata=TestSet)
+confMatSVM <- confusionMatrix(predictSVM, TestSet$classe)
+confMatSVM
+
+# plot matrix results
+plot(confMatSVM$table, col = confMatSVM$byClass, 
+     main = paste("SVM- Accuracy =", round(confMatSVM$overall['Accuracy'], 4)))
